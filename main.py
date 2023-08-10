5a5f4afd6c7a8a481d6ae23a5689d2079cb1f378
admin <cristobalfallascr@hotmail.com>
2023-08-10 11:00:54 -0600

    ended lesson

--- main.py
+++ main.py
@@ -21,4 +21,47 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
+# #You can specify the name of column to read data
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+# total_temp = 0
+# for temp in temp_list:
+#     total_temp += temp
+# avg_temp = total_temp / len(temp_list)
+# print("average temperature: "+str(avg_temp))
+# avg_temp = sum(temp_list)/len(temp_list)
+# print("average temperature: "+str(avg_temp))
+#
+# avg_temp = data["temp"].mean()
+# print("average temperature: "+str(avg_temp))
+# max_temp = data['temp'].max()
+# print("Max temperature: "+str(max_temp))
+#
+# #Get data in Colums
+# print(data['condition'])
+# print(data.condition)
+#
+# #get data in rows
+# print(data[data.day=='Monday'])
+# print("Day with highest temperature: ")
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == 'Monday']
+# celsius = monday.temp
+#
+# farenheit = int(celsius)* 9/5 + 32
+# print(farenheit)
+
+#Create df from scratch
+
+data_dic = {
+    "fruits": ["bananas", "apples","oranges"],
+    "price": [500,1990,2500]
+}
+
+data = pandas.DataFrame(data_dic)
 print(data)
+data.to_csv("fruits-list.csv")
