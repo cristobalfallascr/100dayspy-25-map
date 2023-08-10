7528e65523cfbab423c81613e9ced8bad390ea23
admin <cristobalfallascr@hotmail.com>
2023-08-10 09:47:56 -0600

    added pandas

--- main.py
+++ main.py
@@ -6,14 +6,19 @@
 #         print(data_line.strip())
 #         clean_data.append(data_line.strip())
 #     print(clean_data)
-import csv
+# import csv
 
-with open("weather_data.csv" ) as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        print(row)
-        if row[1] != 'temp':
-            temperatures.append(int(row[1]))
-    print(temperatures)
+#
+# with open("weather_data.csv" ) as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         print(row)
+#         if row[1] != 'temp':
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+#
+import pandas
 
+data = pandas.read_csv("weather_data.csv")
+print(data)
