5bb29ef0a78ec3e8fa1b16460b6dd18af8be1c3f
Cris Fallas <cristobalf@clickitcrlabs.com>
2023-08-21 14:03:03 -0600

    added list comprenhension to game

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ screen.addshape(img)
 turtle.shape(img)
 
 states_coordinates = pandas.read_csv("50_states.csv")
-#print(states_coordinates.state)
+all_states = states_coordinates.state.to_list()
+print(all_states)
 
 def write_state(name, x,y):
     state = turtle.Turtle()
@@ -19,20 +20,29 @@ def write_state(name, x,y):
 
 
 game_is_on = True
+guessed_states = []
 
 while game_is_on:
     answer_state = screen.textinput(title="Guess the State", prompt="Enter State's name:")
+    if answer_state != 'exit':
 
-    match = states_coordinates[states_coordinates.state == answer_state]
-    if len(match) > 0:
-        name = match.iloc[0,0]
-        x = match.iloc[0,1]
-        y = match.iloc[0,2]
-        print(name, x, y)
-        write_state(name,x, y)
+        match = states_coordinates[states_coordinates.state == answer_state]
+        if answer_state in all_states:
+            guessed_states.append(answer_state)
+            name = match.iloc[0,0]
+            x = match.iloc[0,1]
+            y = match.iloc[0,2]
+            print(name, x, y)
+            write_state(name,x, y)
+    else:
+        states_to_learn = [state for state in all_states if state not in guessed_states]
+        game_is_on = False
 
+print("States you know: " + str(len(guessed_states)))
+print(guessed_states)
 
-
+print("States you dont know: " + str(len(states_to_learn)))
+print(states_to_learn)
 
 
 
